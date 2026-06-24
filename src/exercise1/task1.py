f85c7b2eed57b978a0e0cd42eaedab0e75b4f033
kissabmi <123za123g@gmail.com>
2026-06-24 09:04:00 +0700

    task1

---
+++ src/exercise1/task1.py
@@ -0,0 +1,5 @@
+vector1 = list(map(float, input().split()))
+vector2 = list(map(float, input().split()))
+
+result = vector1[0] * vector2[0] + vector1[1] * vector2[1] + vector1[2] * vector2[2]
+print(result)
